87512997e541d81b76363fc51de6ec22a7fd1c61
Sahand Zeinali <szeinali@hawk.iit.edu>
2017-03-12 05:36:26 -0500

    Add files via upload

---
+++ basicNumpy.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# We first create an array named arr and print it
+arr = np.array([1,2,3,4,5,6,7,8,9,10])
+print(arr)
+
+#Print the shape
+print(arr.shape)
+
+#reshape it
+print(arr.reshape(5,2))
