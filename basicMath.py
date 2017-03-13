f61a191b89f0c2376a72149fdc511ee6b4cb156a
Sahand Zeinali <szeinali@hawk.iit.edu>
2017-03-13 18:39:25 -0500

    import math
    using math module, mathematical functions are used here.

---
+++ basicMath.py
@@ -0,0 +1,30 @@
+import math as m
+
+
+# Expected result for square root of 4  = 2
+print(m.sqrt(4))
+
+# Expected result for log2 (4) = 2
+print(m.log2(4))
+
+# Expected result for 2^3 = 8
+print(m.pow(2,3))
+
+# cos takes in radians
+# radians() takens in degrees & outputs radians
+# Expected result for cos(180) = -1
+print(m.cos(m.radians(180)))
+
+# Expected result for sin(90) = 1
+print(m.sin(m.radians(90)))
+
+# Expected result for the ceiling of 3.5 is 4
+print(m.ceil(3.5))
+
+# For floor you can either use // or floor
+# Expected value for 3.5's floor is 3
+print(3.5//1)
+print(m.floor(3.5))
+
+# Expected value of pi
+print(m.pi)
